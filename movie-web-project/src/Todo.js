2600d44cabf5352ebd76212ec9a84a8871aec29e
white-hun <121800818+white-hun@users.noreply.github.com>
2023-02-25 22:37:03 +0900

    Update todo-1

--- movie-web-project/src/Todo.js
+++ movie-web-project/src/Todo.js
@@ -1,16 +1,25 @@
 import { useState } from "react";
 
-function Todo() {
+function ToDo() {
   const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
   const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => event.preventDefault(); // 새로고침 하지않고 -->
-  if (toDo === "") {
-    return;
-  }
-
-  setToDo(""); // --> 값을 비운다
+  const onSubmit = (event) => {
+    event.preventDefault(); // 새로고침 하지않고 -->
+    if (toDo === "") {
+      return;
+    }
+    // 우선 비어있는 currentArray 받아온 후
+    // 새로운 toDos가
+    // input을 통해 작성한 toDo와
+    // 비어있는 array의 element가 더해진다
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo(""); // --> 값을 비운다
+  };
+  console.log(toDos);
   return (
     <div>
+      <h1>My ToDo List ({toDos.length})</h1>
       <form onSubmit={onSubmit}>
         <input
           onChange={onChange}
@@ -24,6 +33,6 @@ function Todo() {
   );
 }
 
-export default Todo;
+export default ToDo;
 
 // form은 submit이라는 이벤트를 갖고 있어서 제출하면 form형식이 처리되고 새로고침된다
--- movie-web-project/src/index.js
+++ movie-web-project/src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import App from "./App";
 // import CleanUp from "./Cleanup";
-import Todo from "./Todo";
+import ToDo from "./ToDo";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Todo />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <ToDo />
+  // </React.StrictMode>
 );
