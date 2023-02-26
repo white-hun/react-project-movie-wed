f25d0e31b5cdf28015e286b1897398cd434aece5
white-hun <121800818+white-hun@users.noreply.github.com>
2023-02-26 16:55:00 +0900

    Update CoinTracker

--- movie-web-project/src/CoinTracker.js
+++ movie-web-project/src/CoinTracker.js
@@ -1,3 +1,42 @@
-function CoinTraker() {}
+import { useEffect, useState } from "react";
+import "./CoinTracker.css";
 
+function CoinTraker() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <div>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <ul>
+          {coins.map((coin) => (
+            <li key={coin.id}>
+              <a
+                rel="noreferrer"
+                href={`https://coinpaprika.com/ko/coin/${coin.id}/`}
+                target="_blank"
+              >
+                {coin.name} ({coin.symbol}):{" "}
+              </a>
+              {Math.round(coin.quotes.USD.price)
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}{" "}
+              USD
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
 export default CoinTraker;
