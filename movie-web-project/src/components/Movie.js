124743014e5432c3a5664803abce8caab1a205fc
white-hun <121800818+white-hun@users.noreply.github.com>
2023-02-27 21:26:22 +0900

    Update gh-pages

--- movie-web-project/src/components/Movie.js
+++ movie-web-project/src/components/Movie.js
@@ -6,18 +6,20 @@ export default function Movie({ id, coverImg, title, year, summary, genres }) {
   return (
     <div className={styles.movie}>
       <img src={coverImg} alt={title} className={styles.movie__img} />
-      <div>
-        <h2 className={styles.movie__title}>
-          <Link to={`/movie/${id}`}>{title}</Link>
-        </h2>
-        <h3 className={styles.movie__year}>{year}</h3>
-        <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
-        <ul className={styles.movie__genres}>
-          {genres.map((g) => (
-            <li key={g}>{g}</li>
-          ))}
-        </ul>
-      </div>
+      <Link to={`/movie/${id}`}>
+        <div className={styles.movie__box}>
+          <h2 className={styles.movie__title}>{title}</h2>
+          <h3 className={styles.movie__year}>{year}</h3>
+          <p>
+            {summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}
+          </p>
+          <ul className={styles.movie__genres}>
+            {genres.map((g) => (
+              <li key={g}>{g}</li>
+            ))}
+          </ul>
+        </div>
+      </Link>
     </div>
   );
 }
