9ed2d93dcf35fa294d46e258fa9b961dbdc56be9
white-hun <121800818+white-hun@users.noreply.github.com>
2023-02-27 19:08:06 +0900

    Update css

--- movie-web-project/src/components/Movie.js
+++ movie-web-project/src/components/Movie.js
@@ -1,22 +1,23 @@
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
-import "./Movie.css";
+import styles from "./Movie.module.css";
 
-export default function Movie({ id, coverImg, title, summary, genres }) {
+export default function Movie({ id, coverImg, title, year, summary, genres }) {
   return (
-    <div>
-      <h2>
-        <Link to={`/movie/${id}`} className="link">
-          {title}
-        </Link>
-      </h2>
-      <img src={coverImg} alt={title} />
-      <p>{summary}</p>
-      <ul>
-        {genres.map((g) => (
-          <li key={g}>{g}</li>
-        ))}
-      </ul>
+    <div className={styles.movie}>
+      <img src={coverImg} alt={title} className={styles.movie__img} />
+      <div>
+        <h2 className={styles.movie__title}>
+          <Link to={`/movie/${id}`}>{title}</Link>
+        </h2>
+        <h3 className={styles.movie__year}>{year}</h3>
+        <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
+        <ul className={styles.movie__genres}>
+          {genres.map((g) => (
+            <li key={g}>{g}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- movie-web-project/src/index.js
+++ movie-web-project/src/index.js
@@ -13,6 +13,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import main from "./Main";
+import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
